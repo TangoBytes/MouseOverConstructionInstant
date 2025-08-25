7a5e46067737c0de4ce6a419a594b5635e109403
Caleb Heuer <ch@raiguard.me>
2025-08-25 01:17:07 -0600

    Remove outdated todo

--- prototypes/shortcut.lua
+++ prototypes/shortcut.lua
@@ -2,7 +2,6 @@ data:extend({
   {
     type = "shortcut",
     name = "moc-toggle",
-    -- TODO: New 56-pixel icon
     icon = "__MouseOverConstruction__/graphics/shortcut-x32.png",
     icon_size = 36,
     small_icon = "__MouseOverConstruction__/graphics/shortcut-x24.png",
